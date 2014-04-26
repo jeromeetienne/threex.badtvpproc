55864cee0b3fea4efff18383b2c8a733aef6e7c1
Jerome Etienne <jerome.etienne@gmail.com>
2014-04-26 02:39:46 +0200

    added mit license from @felixturner in his shader

--- shaders/BadTVShader.js
+++ shaders/BadTVShader.js
@@ -1,5 +1,5 @@
 /**
- * @author felixturner / http://airtight.cc/
+ * @author Felix Turner / www.airtight.cc / @felixturner
  *
  * Bad TV Shader
  * Simulates a bad TV via horizontal distortion and vertical roll
@@ -10,8 +10,30 @@
  * distortion2: amount of fine grain distortion
  * speed: distortion vertical travel speed
  * rollSpeed: vertical roll speed
+ * 
+ * The MIT License
+ * 
+ * Copyright (c) 2014 Felix Turner
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
  */
-
 THREE.BadTVShader = {
 	uniforms: {
 		"tDiffuse": { type: "t", value: null },
